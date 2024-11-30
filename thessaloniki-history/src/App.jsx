5094916bec3d8626d1d7f573ac7bececa3ce9d9c
Anthony Micco <anthonymicco2003@gmail.com>
2024-11-30 18:29:01 +0200

    set up nav bar and began designing page layout

--- thessaloniki-history/src/App.jsx
+++ thessaloniki-history/src/App.jsx
@@ -1,14 +1,40 @@
-import whiteTower from './assets/white-tower.jpg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import References from './components/References';
+import Gallery from './components/Gallery';
 
 function App() {
 
+  const year = new Date().getFullYear()
+
   return (
     <>
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-        <h1>Jewish History of Thessaloniki</h1>
-        <img src={whiteTower} alt="White Tower" width={"1000px"} />
-
-      </div>
+      <Nav></Nav>
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Home />} />
+          <Route
+            path="/References"
+            element={<References />} />
+          <Route
+            path="/Gallery"
+            element={<Gallery />} />
+          <Route
+            path="*"
+            element={<Navigate to="/" />}
+          />
+        </Routes>
+      </Router>
+      <footer> Designed and Developed by Anthony Micco &copy;{year} </footer>
     </>
   )
 }
---
+++ thessaloniki-history/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <h2>How is started</h2>
+            <h2>Impact</h2>
+            <h2>Recovery Efforts</h2>
+        </div>
+    )
+}
