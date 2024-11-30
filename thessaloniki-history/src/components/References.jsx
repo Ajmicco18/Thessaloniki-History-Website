d05f7eba331e4e65d5a1a3bf05f828b0e42f7cc5
Anthony Micco <anthonymicco2003@gmail.com>
2024-11-30 18:55:29 +0200

    added sources to references page

--- thessaloniki-history/src/components/References.jsx
+++ thessaloniki-history/src/components/References.jsx
@@ -4,6 +4,10 @@ export default function References() {
     return (
         <div>
             <h1>References Page</h1>
+            <p>Mazower, Mark. Salonica, City of Ghosts: Christians, Muslims and Jews. Harper Perennial, 2016. </p>
+            <a href='https://www.greece-is.com/from-ashes-to-rebirth-the-great-fire-of-thessaloniki/'>
+                Yerolymbos, Alexandra. “From Ashes to Rebirth: The Great Fire of Thessaloniki.” Greece-Is, 16 Jan. 2016, www.greece-is.com/from-ashes-to-rebirth-the-great-fire-of-thessaloniki/.
+            </a>
         </div>
     )
 }
