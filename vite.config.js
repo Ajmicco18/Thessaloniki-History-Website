82c42f8dd248d4f0e79f8791b7d0d17081619438
Anthony Micco <anthonymicco2003@gmail.com>
2024-12-03 12:32:56 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Thessaloniki-History-Website/',
   plugins: [react()],
+  base: "/thessaloniki-fire",
 })
